ef89abd9b89b2be95c782d16be33101a7e06227d
boguszmedykowski <bogusz.medykowski@gmail.com>
2023-11-25 21:13:06 +0100

    utworzenie bazy danych i podstawowego API

--- main.py
+++ main.py
@@ -6,13 +6,40 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///budget.db'
 db = SQLAlchemy(app)
 
 
-# Inicjalizacja bazy danych w kontekście aplikacji
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    type = db.Column(db.String(10), nullable=False)  # Expense or Income
 
+
+# Inicjalizacja bazy danych w kontekście aplikacji
+with app.app_context():
+    db.create_all()
 
 # Strona główna z formularzem do dodawania transakcji
 
 
+@app.route('/')
+def index():
+    transactions = Transaction.query.all()
+    return render_template('index.html', transactions=transactions)
+
 # Dodawanie nowej transakcji
+
+
+@app.route('/add_transaction', methods=['POST'])
+def add_transaction():
+    title = request.form['title']
+    amount = float(request.form['amount'])
+    type = request.form['type']
+
+    new_transaction = Transaction(title=title, amount=amount, type=type)
+
+    db.session.add(new_transaction)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
