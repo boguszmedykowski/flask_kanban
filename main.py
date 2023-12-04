d709c085c056b929adb48808988c9d096bb59b8b
p3wv <memiarzyskrzynia@gmail.com>
2023-12-04 19:58:57 +0100

    add register/login forms

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, Response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import SQLAlchemyError
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
 from enum import Enum
 import io
 import csv
@@ -14,6 +19,8 @@ class Config:
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+bcrypt = Bcrypt(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
@@ -33,6 +40,44 @@ class Transaction(db.Model):
     amount = db.Column(db.Float, nullable=False)
     type = db.Column(db.Enum(TransactionType), nullable=False)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+     return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+             raise ValidationError(
+                  "There already is a user with that username. Choose a different one.")
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Username"})
+    
+    password = PasswordField(validators=[InputRequired(), Length(
+        min=4, max=20)], render_kw={"placeholder": "Password"})
+    
+    submit = SubmitField("Login")
 
 # Inicjalizacja bazy danych
 with app.app_context():
@@ -48,6 +93,34 @@ def index():
 
 # Dodawanie transakcji
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+         hashed_password = bcrypt.generate_password_hash(form.password.data)
+         new_user = User(username=form.username.data, password=hashed_password)
+         db.session.add(new_user)
+         db.session.commit()
+         return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+         user = User.query.filter_by(username=form.username.data).first()
+         if user:
+              if bcrypt.check_password_hash(user.password, form.password.data):
+                   login_user(user)
+                   return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
+
+@app.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+     return render_template('dashboard.html')
 
 @app.route('/add_transaction', methods=['GET', 'POST'])
 def add_transaction():
